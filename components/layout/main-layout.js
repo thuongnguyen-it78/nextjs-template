4b418c8e64440ba590b1117c85b287dbb535291a
Andres Nguyen <thuongnguyen.it78@gmail.com>
2022-12-27 16:00:37 +0700

    update code

--- components/layout/main-layout.js
+++ components/layout/main-layout.js
@@ -1,11 +1,11 @@
-import { Footer, Header } from "../common";
+import { Footer, Header } from '../common';
 
 export function MainLayout({ children }) {
-  return (
-    <>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<main>{children}</main>
+			<Footer />
+		</>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
 export default function Home() {
-  return <Fragment>Home Page</Fragment>;
+	return <Fragment>Home Page</Fragment>;
 }
